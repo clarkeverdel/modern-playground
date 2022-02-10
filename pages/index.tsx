05c5897b9301796465531fb8156ae5adb94d3481
Clarke Verdel <info@claver-webdesign.nl>
2022-02-10 20:55:52 +0100

    feat(): Add homepage as test & custom App file to load styles

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>Clarke&apos;s Modern Playground</h1>
+      <h2 className="subtitle">Let&apos;s get your IT ready for battle</h2>
+      <p>Your partner for awesome digital solutions</p>
+    </>
+  );
+};
+
+export default HomePage;
